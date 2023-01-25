da836f0766677d111d38a04c3e09fcbf8dab1e0a
frederickcgz <66274509+frederickcgz@users.noreply.github.com>
2023-01-24 23:42:05 -0500

    Update card.js

--- card.js
+++ card.js
@@ -1,92 +1,95 @@
-let cardNumber = document.getElementById("number");
-let numberInp = document.getElementById("card_number");
-let nameInp = document.getElementById("card_name");
-let cardName = document.getElementById("name");
-let cardMonth = document.getElementById("month");
-let cardYear = document.getElementById("year");
-let monthInp = document.getElementById("card_month");
-let yearInp = document.getElementById("card_year");
-let cardCvc = document.getElementById("cvc");
-let cvcInp = document.getElementById("card_cvc");
-let submitBtn = document.getElementById("submit_btn");
-let compeleted = document.querySelector(".thank");
-let form = document.querySelector("form");
+const numberInput = document.getElementById("card_number"),
+      nameInput = document.getElementById("card_name"),
+      monthInput = document.getElementById("card_month"),
+      yearInput = document.getElementById("card_year"),
+      cvcInput = document.getElementById("card_cvc"),
+      cardNumber = document.getElementById("number"),
+      cardName = document.getElementById("name"),
+      cardMonth = document.getElementById("month"),
+      cardYear = document.getElementById("year"),
+      cardCvc = document.getElementById("cvc"),
+      submitBtn = document.getElementById("submit_btn"),
+      completed = document.querySelector(".thank"),
+      form = document.querySelector("form");
 
-function setCardNumber(e) {
-    cardNumber.innerText = format(e.target.value);
-}
-function setCardName(e) {
-  cardName.innerText = format(e.target.value);
-}
-function setCardMonth(e) {
-  cardMonth.innerText = format(e.target.value);
-}
-function setCardYear(e) {
-  cardYear.innerText = format(e.target.value);
-}
-function setCardCvc(e) {
-  cardCvc.innerText = format(e.target.value);
+
+function setCardData(e) {
+    let target = e.target;
+    let id = target.id;
+    let value = target.value;
+    let cardField = document.getElementById(id.replace("card_", ""));
+    cardField.innerText = format(value);
 }
 
 function handleSubmit(e) {
-    e.preventDefault();
-    if (!nameInp.value) {
-      nameInp.classList.add('error');
-      nameInp.parentElement.classList.add("error_message")
-    } else {
-      nameInp.classList.remove("error");
-      nameInp.parentElement.classList.remove("error_message");
-    }
-    if (!numberInp.value) {
-      numberInp.classList.add('error')
-      numberInp.parentElement.classList.add("error_message");
-    } else if (numberInp.value.length < 16) {
-        numberInp.classList.add("error")
-    } else {
-      numberInp.classList.remove("error");
-      numberInp.parentElement.classList.remove("error_message");
-    }
-    if (!monthInp.value) {
-      monthInp.classList.add("error")
-      monthInp.parentElement.classList.add("error_message");
-    } else {
-      monthInp.classList.remove("error");
-      monthInp.parentElement.classList.remove("error_message");
-    }
-    if (!yearInp.value) {
-      yearInp.classList.add("error")
-      yearInp.parentElement.classList.add("error_message");
-    } else {
-      yearInp.classList.remove("error");
-      yearInp.parentElement.classList.remove("error_message");
-    }
-    if (!cvcInp.value) {
-      cvcInp.classList.add("error")
-      cvcInp.parentElement.classList.add("error_message");
-    } else {
-      cvcInp.classList.remove("error");
-      cvcInp.parentElement.classList.remove("error_message");
-    }
-    if (
-      nameInp.value &&
-      numberInp.value &&
-      monthInp.value &&
-      yearInp.value &&
-      cvcInp.value &&
-      numberInp.value.length == 16
-    ) {
-      compeleted.classList.remove("hidden");
+  e.preventDefault();
+  let isValid = true;
+  const inputs = [nameInput, numberInput, monthInput, yearInput, cvcInput];
+  inputs.forEach(input => {
+      switch (input.id) {
+          case "card_name":
+              if (!input.value) {
+                  input.classList.add("error");
+                  input.parentElement.classList.add("error_message");
+                  isValid = false;
+              } else {
+                  input.classList.remove("error");
+                  input.parentElement.classList.remove("error_message");
+              }
+              break;
+          case "card_number":
+              if (!input.value || input.value.length < 16) {
+                  input.classList.add("error");
+                  input.parentElement.classList.add("error_message");
+                  isValid = false;
+              } else {
+                  input.classList.remove("error");
+                  input.parentElement.classList.remove("error_message");
+              }
+              break;
+          case "card_month":
+          case "card_year":
+          case "card_cvc":
+              if (!input.value) {
+                  input.classList.add("error");
+                  input.parentElement.classList.add("error_message");
+                  isValid = false;
+              } else {
+                  input.classList.remove("error");
+                  input.parentElement.classList.remove("error_message");
+              }
+              break;
+          default:
+              break;
+      }
+  });
+  if (isValid) {
+      completed.classList.remove("hidden");
       form.classList.add("hidden");
-    }
-
+  }
 }
+
+
 function format(s) {
-  return s.toString().replace(/\d{4}(?=.)/g, "$& ");
+return s.toString().replace(/\d{4}(?=.)/g, "$& ");
 }
 
-numberInp.addEventListener("keyup", setCardNumber);
-nameInp.addEventListener("keyup", setCardName);
-monthInp.addEventListener("keyup", setCardMonth);
-yearInp.addEventListener("keyup", setCardYear);
-cvcInp.addEventListener("keyup", setCardCvc);
-submitBtn.addEventListener("click", handleSubmit);
+const inputs = [
+  {input: numberInput, output: cardNumber, event: "keyup"},
+  {input: nameInput, output: cardName, event: "keyup"},
+  {input: monthInput, output: cardMonth, event: "keyup"},
+  {input: yearInput, output: cardYear, event: "keyup"},
+  {input: cvcInput, output: cardCvc, event: "keyup"},
+  {input: submitBtn, output: null, event: "click"}
+];
+
+inputs.forEach(item => {
+  if (item.event === "keyup") {
+      item.input.addEventListener(item.event, function(e) {
+          item.output.innerText = format(e.target.value);
+      });
+  } else {
+      item.input.addEventListener(item.event, handleSubmit);
+  }
+});
+
